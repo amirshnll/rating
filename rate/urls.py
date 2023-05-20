bd62becae5385abedd6b36b126c6d5c88502e8aa
Amir Shokri <amirsh.nll@gmail.com>
2023-05-20 13:58:54 +0330

    add : rate view

--- rate/urls.py
+++ rate/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import RateApi
+from .views import RateApi, RateListApi, TotalRatePostApi
 
 urlpatterns = [
     path("", RateApi.as_view()),
+    path("<int:rate_id>/", RateApi.as_view()),
+    path("list/", RateListApi.as_view()),
+    path("total/<str:blog_post_id>/", TotalRatePostApi.as_view()),
 ]
--- rate/views.py
+++ rate/views.py
@@ -1,1 +1,46 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from user.permissions import method_permission_classes, IsAuthor, IsLogginedUser
+from .serializers import RatingSerializer
+from .models import Rating as RatingModel
+
+
+class RateApi(APIView):
+    @method_permission_classes([IsLogginedUser])
+    def post(self, request):
+        rate_serializer = RatingSerializer(data=request.data)
+        if not rate_serializer.is_valid():
+            return Response(rate_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        rate_serializer.save()
+        return Response(rate_serializer.data, status=status.HTTP_201_CREATED)
+
+    def get(self, request, rate_id):
+        try:
+            rate_obj = RatingModel.objects.get(pk=rate_id)
+            rate_serializer = RatingModel(instance=rate_obj, many=False).data
+            return Response(rate_serializer, status=status.HTTP_200_OK)
+        except RatingModel.DoesNotExist:
+            return Response(
+                {"status": "error", "message": "the post could not be found"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+
+class RateListApi(APIView):
+    @method_permission_classes([IsLogginedUser])
+    def get(self, request):
+        rate_obj = RatingModel.objects.all().order_by("id")
+        rate_serializer = RatingSerializer(instance=rate_obj, many=True).data
+        return Response(rate_serializer, status=status.HTTP_200_OK)
+
+
+class TotalRatePostApi(APIView):
+    @method_permission_classes([IsLogginedUser])
+    def get(self, request, blog_post_id):
+        rate_obj = RatingModel.objects.filter(post=blog_post_id).order_by("id")
+        rate_serializer = RatingSerializer(instance=rate_obj, many=True).data
+        total_rate = sum(rate["value"] for rate in rate_serializer)
+        return Response(total_rate, status=status.HTTP_200_OK)
